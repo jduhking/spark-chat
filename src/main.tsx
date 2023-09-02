60033cef794de9821c86d7f0f64d1459238981ad
jduhking <jamesbodebiyi@gmail.com>
2023-09-02 13:16:23 -0500

    main: initialized git repo

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HomePage from './routes/HomePage.tsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/routes/HomePage.tsx
@@ -0,0 +1,10 @@
+
+export default function HomePage() {
+
+  return (
+    <>
+      <h1>This is the home page man</h1>
+    </>
+  )
+}
+

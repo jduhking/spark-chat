2dd039d0fdd20e0cd40ef274e905b65c57884d37
Ohiani1 <ohianiabdulkadir7@gmail.com>
2023-09-02 20:35:40 -0500

    added a function to test out state

--- frontend/src/routes/HomePage.tsx
+++ frontend/src/routes/HomePage.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import axios from 'axios';
+
+
 
 export default function HomePage() {
 
+  const[demoText, setDemoText] = useState('This is the home page man');
+  const url = '/';
+
+  const handlePress = async () => {
+    try {
+      const result = await axios.get(url);
+      setDemoText(result.data);
+      
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
   return (
     <>
-      <h1>This is the home page man</h1>
+      <h1>{demoText}</h1>
+      <button/>
     </>
   )
 }

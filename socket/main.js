565c6dc4f66a218aa5f709fb327d82c4df6242c6
jduhking <jamesbodebiyi@gmail.com>
2023-09-07 17:14:06 -0500

    one on one chat is mostly complete

--- socket/main.js
+++ socket/main.js
@@ -1,46 +1,41 @@
 const express = require('express');
-const url = require('url')
+
 const ws = require('ws');
 
 const app = express();
+app.use(express.static('public'))
 
 const wss1 = new ws.Server({ noServer: true });
-const wss2 = new ws.Server({ noServer: true });
 
-wss1.on('connection', socket => {
-  socket.on('message', (message) => {
-    console.log(`Message from client1 ${message}`)
+wss1.on('connection', client => {
+    console.log('a client has connected')
+    
+    client.on('message', (message) => {
+    console.log(`Message from a client: \n ${message}`)
+    broadcast(message)
   })
 });
-
-wss2.on('connection', socket => {
-    socket.on('message', (message) => {
-      console.log(`Message from client2 ${message}`)
-    })
-  });
 
 const server = app.listen(8080, () => {
     console.log('App listening of port 8080')
 });
 server.on('upgrade', (req, socket, head) => {
 
-    const { pathname } = url.parse(req.url)
-    console.log(pathname)
-    if(pathname === '/client1'){
-        console.log('connecting client 1')
-        wss1.handleUpgrade(req, socket, head, socket => {
-            wss1.emit('connection', socket, req)
-        });
+    wss1.handleUpgrade(req, socket, head, socket => {
+        wss1.emit('connection', socket, req)
+    });
 
-    } else if (pathname === '/client2') {
-        console.log('connecting client 2')
-        wss2.handleUpgrade(req, socket, head, socket => {
-            wss2.emit('connection', socket, req)
-        })
-    }
 
-   
 });
+
+function broadcast(msg) {
+    for(const client of wss1.clients){
+        if (client.readyState === ws.OPEN){
+            client.send(msg)
+        }
+        
+    }
+}
 
 app.get('/client1', (req, res) => {
     res.sendFile(__dirname + '/src/client1.html')    
